e590554de3a8998e4b3d672b76b7734b76699d93
ThakaneMokose <thakanemokose97@gmail.com>
2025-06-13 16:03:36 +0200

    create a webpage with a 16x16 grid of square divs

---
+++ script.js
@@ -0,0 +1,15 @@
+const container = document.querySelector(".container");
+
+for(let x = 0; x < 16; x++){
+    const row = document.createElement("div");
+    row.classList.add("row");
+    
+    for(let y = 0; y < 16; y++){
+        const col = document.createElement("div");
+        col.classList.add("col");
+        row.appendChild(col);
+    }
+
+    container.appendChild(row);
+}
+
